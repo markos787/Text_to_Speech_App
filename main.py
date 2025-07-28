7d433e27576c3c54bc8e56f07841434740871518
markos787 <marekweis01@gmail.com>
2025-07-28 18:13:53 +0200

    wip: combo boxes

--- main.py
+++ main.py
@@ -23,6 +23,16 @@ Label(frame_top, image=logo, bg='white').place(x=10, y=5)
 
 Label(frame_top, text='TEXT TO SPEECH',font='arial 20 bold', bg='white', fg='black').place(x=100, y=35)
 
-root.mainloop()
+# Parameters
+text_input=Text(root, font='Robote 20', bg='white', relief=GROOVE, wrap=WORD) # relief - bordering (GROOVE - slightly 3D frame), WORD - wrapping all words
+text_input.place(x=20, y=150, width=500, height=250)
 
-# Film przerwany na: 9:50
+gender=Combobox(root, values=['Male', 'Female'], font='arial 14', state='readonly', width=10)
+gender.place(x=550, y=200)
+gender.set('Male')
+
+speed=Combobox(root, values=['Fast', 'Normal', 'Slow'], font='arial 14', state='readonly', width=10)
+speed.place(x=730, y=200)
+speed.set('Normal')
+
+root.mainloop()
